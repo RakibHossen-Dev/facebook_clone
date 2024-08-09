772413ea98b9b5b40f9dfae06da099b347368708
Rakib <hellorakibhossen@gmail.com>
2024-08-08 22:44:18 -0700

    added setting option

---
+++ index.js
@@ -0,0 +1,6 @@
+let clickphoto = document.getElementById("click-photo");
+let clicgoption = document.getElementById("clicg-option");
+
+clickphoto.addEventListener("click", () => {
+  clicgoption.classList.toggle("hidden");
+});
